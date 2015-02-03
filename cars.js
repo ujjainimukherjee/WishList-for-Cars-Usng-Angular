36bd8e0be218f15dfc261a52c4cbeb4a795d98de
ujjainimukherjee <c_ujjaini@yahoo.com>
2015-02-02 17:29:26 -0800

    AngularJS example with wishlist

---
+++ cars.js
@@ -0,0 +1,58 @@
+var dreamCars = [
+  {
+    make: "Aston Martin",
+    model: "DB9"
+  },
+  {
+    make: "Lotus",
+    model: "Elise"
+  },
+  {
+    make: "Maserati",
+    model: "Quattroporte"
+  },
+  {
+    make: "Porsche",
+    model: "Panamera"
+  },
+  {
+    make: "Aston Martin",
+    model: "Vanquish"
+  },
+  {
+    make: "Maserati",
+    model: "GranTurismo"
+  },
+  {
+    make: "Maserati",
+    model: "GranCabrio"
+  },
+  {
+    make: "Lotus",
+    model: "Exige"
+  },
+  {
+    make: "Aston Martin",
+    model: "Vantage"
+  },
+  {
+    make: "Porsche",
+    model: "911"
+  },
+  {
+    make: "Bugatti",
+    model: "Veyron"
+  },
+  {
+    make: "Bugatti",
+    model: "Galibier"
+  },
+  {
+    make: "Lotus",
+    model: "Evora"
+  },
+  {
+    make: "Lotus",
+    model: "2-eleven"
+  },
+];
